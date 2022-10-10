d2dbc7dc87eee0c6bb293272edc6e50385b804a6
sannyralph <sannylauren@gmail.com>
2022-10-10 22:43:10 +0200

    const var added to js

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operatior]")
+const deleteButton = document.querySelector("[data-delete]")
+const equalButton = document.querySelector("[data-equal]")
+const allclearButton = document.querySelectorAll("[data-all-clear]")
+const returnButton = document.querySelectorAll("[data-return]")
+const inputButton = document.querySelectorAll("[data-input]")
